f22ce0394783ac7632edf11f1d676654b41dd757
jack <jhc33@wildcats.unh.edu>
2021-02-22 15:27:31 -0500

    include plot locations

--- inventory_N_NIR.R
+++ inventory_N_NIR.R
@@ -11,20 +11,25 @@
 #########
 
 library(tidyverse)
+library(sf)
 
 
 ########
 #Import
 #######
 
-spp_codes <- readr::read_csv('data_files/BEF_sppCodes.csv')
-BEF_inventory <- readr::read_csv('data_files/BEF_Inventory_Overstory_2001-03.csv')
+# BEF species ID codes and inventory data
+spp_codes <- read_csv('data_files/BEF_sppCodes.csv')
+BEF_inventory <- read_csv('data_files/BEF_Inventory_Overstory_2001-03.csv', guess_max = 3000)
+
+#BEF plot coordinates; 400+ plots 
+plot_loc <- read_sf('data_files/BEFplots/BEFInventory_Extents_UTM.shp')
 
 #merge
 inventory_merge <- BEF_inventory %>%
-  dplyr::left_join(spp_codes, by = c('Spp', 'Spp_Old'))
+  left_join(spp_codes, by = c('Spp', 'Spp_Old'))
 
 #paired down -- inventory. Revisit as needed
 inven <- inventory_merge %>%
-  dplyr::select(c('Plot_Number','Common_Name', 'DBH_Class', 'Plot_Area'))
+  select(c('Plot_Number','Common_Name', 'DBH_Class', 'Plot_Area'))
 
